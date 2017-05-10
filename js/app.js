f6e4a4eb13eefdb57467af12d3c2d8434f07f66e
Miriam <miriammp1997@gmail.com>
2017-05-10 13:59:07 -0500

    add modal

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
-/*
- * Archivo principal de JS
- */
+window.addEventListener("load", function(e){
+  e.preventDefault();
+
+})
+var p = document.getElementsByClassName("port-imagen");
+for(var i= 0 ; i< p.length; i++){
+  p[i].addEventListener("click", screenSize);
+  p[i].addEventListener("mouseover", function(){
+    this.firstChild.classList.add("visible")
+  });
+  p[i].addEventListener("mouseout", function(){
+    this.firstChild.classList.remove("visible")
+  });
+}
+
+function screenSize(){
+  document.getElementById("modal").firstElementChild.classList.add("modal");
+  console.log(this.previousElementSibling);
+}
