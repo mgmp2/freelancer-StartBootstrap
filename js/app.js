123a74537c51e62bf4f23c16dbc1b41ea935886c
Miriam <miriammp1997@gmail.com>
2017-05-11 17:49:37 -0500

    add color in list

--- js/app.js
+++ js/app.js
@@ -1,7 +1,44 @@
-window.addEventListener("load", function(e){
-  e.preventDefault();
 
+
+var lastScrollTop = 100;
+var a = document.getElementsByClassName("menu-link");
+window.addEventListener("load", function (e){
+  e.preventDefault();
 })
+window.addEventListener("scroll", function(){
+  var currentScroll = window.scrollY;
+  if (currentScroll > lastScrollTop){
+    document.getElementById("header").classList.add("header-height");
+  } else {
+
+    document.getElementById("header").classList.remove("header-height");
+
+
+    if( currentScroll >= 420 && currentScroll < 1160) {
+      document.getElementById("li-portfolio").classList.add("active");
+      document.getElementById("li-about").classList.remove("active");
+
+
+    } if(currentScroll >= 1160) {
+      document.getElementById("li-about").classList.add("active");
+      document.getElementById("li-portfolio").classList.remove("active");
+      document.getElementById("li-contact").classList.remove("active");
+
+
+
+    } if (currentScroll >= 1540) {
+      document.getElementById("li-contact").classList.add("active");
+      document.getElementById("li-about").classList.remove("active");
+
+
+    }
+  }
+
+   lastScrollTop = currentScroll;
+});
+
+
+
 var p = document.getElementsByClassName("port-imagen");
 for(var i= 0 ; i< p.length; i++){
   p[i].addEventListener("click", screenSize);
