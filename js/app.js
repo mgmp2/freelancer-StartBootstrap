7ac4647aa6e517e4fd3ea1becefdccc4c808823e
Miriam <miriammp1997@gmail.com>
2017-05-11 18:11:40 -0500

    style to hr

--- js/app.js
+++ js/app.js
@@ -7,32 +7,27 @@ window.addEventListener("load", function (e){
 })
 window.addEventListener("scroll", function(){
   var currentScroll = window.scrollY;
-  if (currentScroll > lastScrollTop){
-    document.getElementById("header").classList.add("header-height");
-  } else {
+  if (currentScroll < 100) {
+  document.getElementById("header").classList.remove("header-height");
+} if (currentScroll > lastScrollTop){
+  document.getElementById("header").classList.add("header-height");
+  document.getElementById("li-portfolio").classList.remove("active");
 
-    document.getElementById("header").classList.remove("header-height");
-
-
-    if( currentScroll >= 420 && currentScroll < 1160) {
+} if( currentScroll >= 420 && currentScroll < 1160) {
       document.getElementById("li-portfolio").classList.add("active");
       document.getElementById("li-about").classList.remove("active");
+} if(currentScroll >= 1160) {
+  document.getElementById("li-about").classList.add("active");
+  document.getElementById("li-portfolio").classList.remove("active");
+  document.getElementById("li-contact").classList.remove("active");
+
+} if (currentScroll >= 1540) {
+  document.getElementById("li-contact").classList.add("active");
+  document.getElementById("li-about").classList.remove("active");
 
 
-    } if(currentScroll >= 1160) {
-      document.getElementById("li-about").classList.add("active");
-      document.getElementById("li-portfolio").classList.remove("active");
-      document.getElementById("li-contact").classList.remove("active");
+}
 
-
-
-    } if (currentScroll >= 1540) {
-      document.getElementById("li-contact").classList.add("active");
-      document.getElementById("li-about").classList.remove("active");
-
-
-    }
-  }
 
    lastScrollTop = currentScroll;
 });
